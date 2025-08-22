1258784cdc52592ae94cd9e16970fbc82c8a2e47
Noah <noahjames1035@gmail.com>
2025-08-22 17:06:44 -0400

    book add spacing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const books = document.querySelector(".books");
 const addBtn = document.querySelector(".add__btn");
 const deleteBtn = document.querySelector(".delete__book--btn");
 const dialog = document.querySelector("dialog");
@@ -48,3 +49,93 @@ function Book(title, author, pages, read) {
 Book.prototype.toggleStatus = function () {
   this.read = !this.read;
 };
+
+function createBookCard(book, bookId) {
+  const newCard = document.createElement("div");
+  newCard.classList.add("book__info--wrapper");
+  newCard.id = bookId;
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("book__info");
+  newCard.appendChild(cardContent);
+
+  const title = document.createElement("div");
+  title.textContent = `${book.title}`;
+  title.classList.add("book__title");
+  cardContent.appendChild(title);
+
+  const author = document.createElement("div");
+  author.textContent = `By: ${book.author}`;
+  author.classList.add("book__author");
+  cardContent.appendChild(author);
+
+  const pages = document.createElement("div");
+  pages.textContent =`Pages: ${book.pages} pages`;
+  pages.classList.add("book__pages");
+  cardContent.appendChild(pages);
+
+  const readStatus = document.createElement("div");
+  readStatus.classList.add("book__read--status");
+  readStatus.appendChild(document.createTextNode("Status: "));
+
+  const statusValue = document.createElement("span");
+  statusValue.classList.add("book__status");
+  statusValue.textContent = book.read ? "Finished" || "In Progress" : "Not Started";
+  readStatus.appendChild(statusValue);
+  cardContent.appendChild(readStatus);
+
+  const cardActions = document.createElement("div");
+  cardActions.classList.add("card-actions");
+  newCard.appendChild(cardActions);
+
+  const switchStatusBtn = document.createElement("button");
+  switchStatusBtn.textContent = "Change Read Status";
+  switchStatusBtn.classList.add("edit__btn");
+  cardActions.appendChild(switchStatusBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete Book";
+  deleteBtn.classList.add("edit__btn");
+  cardActions.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", (event) => {
+    const parentCard = event.target.closest(".card");
+    const bookId = parentCard.id;
+    myLibrary = myLibrary.filter((book) => book.id !== bookId);
+    displayLibrary();
+  });
+
+  switchStatusBtn.addEventListener("click", () => {
+    book.toggleStatus();
+    displayLibrary();
+  });
+
+  return newCard;
+}
+deleteBtn.addEventListener("click", (event) => {
+  const parentCard = event.target.closest(".book__info--wrapper");
+  const bookId = parentCard.id;
+  myLibrary = myLibrary.filter((book) => book.id !== bookId);
+  displayLibrary();
+});
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+
+  displayLibrary();
+}
+
+function displayLibrary() {
+  books.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookId = book.id;
+    const newBookCard = createBookCard(book, bookId);
+    books.appendChild(newBookCard);
+  });
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", true);
+
+myLibrary.push(hobbit);
+
+displayLibrary(myLibrary);
